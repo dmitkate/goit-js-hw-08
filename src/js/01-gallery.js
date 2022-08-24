51536e97f043e36cdd215147e0611b243dbdbb6b
dmitkate <44602492+dmitkate@users.noreply.github.com>
2022-08-24 19:49:55 +0300

    add first

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
-
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const galleryItem = galleryItems.map((item) =>
+    `<a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+        </a>`)
+    .join("");
+
+gallery.innerHTML = galleryItem;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    overlayOpacity: 1,
+    captionsData: "alt",
+    captionDelay: 250,
+    });
